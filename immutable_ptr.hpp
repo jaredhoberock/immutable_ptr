9fa614d03eccad50efcd212078f7d7204ae8093e
Jared Hoberock <jaredhoberock@gmail.com>
2012-09-06 15:46:22 -0700

    Fix compilation errors and WAR a problem with Thrust.

--- immutable_ptr.hpp
+++ immutable_ptr.hpp
@@ -3,22 +3,47 @@
 #include <thrust/memory.h>
 #include <thrust/system/cuda/memory.h>
 
+template<typename Element> class immutable_ptr;
 template<typename Element> class immutable_reference;
+
+namespace thrust
+{
+namespace detail
+{
+
+// XXX iterator_facade tries to instantiate the Reference
+//     type when computing the answer to is_convertible<Reference,Value>
+//     we can't do that at that point because immutable_reference
+//     is not complete
+//     WAR the problem by specializing is_convertible
+template<typename T>
+  struct is_convertible<immutable_reference<T>, T>
+    : thrust::detail::true_type
+{};
+
+} // end detail
+} // end thrust
 
 template<typename Element>
   class immutable_ptr
-    : public thrust::pointer<const Element,
-                             thrust::cuda::tag,
-                             immutable_reference<Element>,
-                             immutable_ptr<Element>
+    : public thrust::pointer<
+               //const Element, // XXX WAR Thrust #227
+               Element,
+               thrust::cuda::tag,
+               immutable_reference<Element>,
+               immutable_ptr<Element>
+             >
 {
   private:
     typedef thrust::pointer<
-      Element,
+      //const Element,
+      Element, // XXX WAR Thrust #227
       thrust::cuda::tag,
-      immutable_reference<T>,
+      immutable_reference<Element>,
       immutable_ptr<Element>
     > super_t;
+
+    typedef immutable_ptr<Element> pointer;
 
   public:
     __host__ __device__
@@ -27,7 +52,9 @@ template<typename Element>
     template<typename OtherElement>
     __host__ __device__
     explicit immutable_ptr(OtherElement *ptr)
-      : super_t(ptr)
+    //  : super_t(ptr)
+    //  XXX WAR Thrust #227
+      : super_t(const_cast<Element*>(ptr))
     {}
 
     template<typename OtherPointer>
@@ -53,6 +80,15 @@ template<typename Element>
     }
 };
 
+
+template<typename T>
+inline __host__ __device__
+immutable_ptr<T> make_immutable(const T *ptr)
+{
+  return immutable_ptr<T>(ptr);
+}
+
+
 template<typename Element>
   class immutable_reference
     : public thrust::reference<
@@ -66,39 +102,46 @@ template<typename Element>
       const Element,
       immutable_ptr<Element>,
       immutable_reference<Element>
-    > type;
+    > super_t;
 
   public:
-    typedef Pointer                                              pointer;
+    typedef immutable_ptr<Element>                               pointer;
     typedef typename thrust::detail::remove_const<Element>::type value_type;
 
-    __host__ __device__
+    inline __host__ __device__
     explicit immutable_reference(const pointer &ptr)
       : super_t(ptr)
     {}
 
     template<typename OtherT>
-    __host__ __device__
+    inline __host__ __device__
     immutable_reference(const immutable_reference<OtherT> &other,
                         typename thrust::detail::enable_if_convertible<
-                          typename reference<OtherT>::pointer,
+                          typename immutable_reference<OtherT>::pointer,
                           pointer
                         >::type * = 0)
       : super_t(other)
     {}
 
-    __host__ __device__
-    operator value_type () const;
+    inline __host__ __device__
+    operator value_type () const
+    {
+    #if __CUDA_ARCH__ >= 350
+      // use streaming load
+      return __ldg(thrust::raw_pointer_cast(&(*this)));
+    #else
+      // defer to super_t otherwise
+      return super_t::operator Element ();
+    #endif
+    };
 
   private:
     // can't assign to an immutable_reference
     template<typename OtherT>
-    __host__ __device__
-    reference &operator=(const reference<OtherT> &other);
+    inline __host__ __device__
+    immutable_reference &operator=(const immutable_reference<OtherT> &other);
 
-    __host__ __device__
-    reference &operator=(const value_type &x);
+    inline __host__ __device__
+    immutable_reference &operator=(const value_type &x);
 };
-
-#include "immutable_ptr.hpp"
 
